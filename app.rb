9ea6ab730bf418896a6e40bdc22547d98ad5818d
Cory Spitzer & Franz Knupfer <coryspitzer@gmail.com>
2015-05-13 15:44:01 -0700

    Add form to add ingredients to a recipe

--- app.rb
+++ app.rb
@@ -22,3 +22,18 @@ post('/recipes') do
   @recipe = Recipe.create(name: recipe_name, instructions: instructions)
   redirect('/recipes')
 end
+
+get('/recipe/:id') do
+  @recipe = Recipe.find(params.fetch('id'))
+  @ingredients = Ingredient.all()
+  erb(:single_recipe)
+end
+
+post('/recipe/:id') do
+  id = params.fetch('id')
+  ingredient_name = params.fetch('ingredient_name')
+  @ingredient = Ingredient.create(name: ingredient_name)
+  recipe = Recipe.find(id)
+  recipe.ingredients.push(@ingredient)
+  redirect("recipe/#{id}")
+end
