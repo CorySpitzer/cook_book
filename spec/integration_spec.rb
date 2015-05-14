128a9942389a08bca19145e405403f4fc49d4da4
Cory Spitzer & Ben Cornelis <student@epicodus.com>
2015-05-14 09:34:36 -0700

    Test integration for the add recipe path

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,4 +12,13 @@ describe('the cookbook app', type: :feature) do
       expect(page).to have_content('Cookbook')
     end
   end
+
+  it('the add a new recipe form path') do
+    visit('/recipe_form')
+    fill_in('recipe_name', with: 'bread')
+    fill_in('instructions', with: 'bake it')
+    click_button('Add')
+    expect(page).to have_content('bread')
+  end
+  
 end
